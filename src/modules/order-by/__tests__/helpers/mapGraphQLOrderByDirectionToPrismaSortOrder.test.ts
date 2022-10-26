0a17f860c57d16ea58a6d6454ed4caa45e24c804
rconrardy <conrardy@umich.edu>
2022-10-25 21:50:13 -0400

    Add ability to handle null and undefined in order by direction.

--- src/modules/order-by/__tests__/helpers/mapGraphQLOrderByDirectionToPrismaSortOrder.test.ts
+++ src/modules/order-by/__tests__/helpers/mapGraphQLOrderByDirectionToPrismaSortOrder.test.ts
@@ -2,26 +2,48 @@ import { OrderByDirection } from '@jest-games-organization/backend-package-graph
 import { mapGraphQLOrderByDirectionToPrismaSortOrder } from '../../helpers/mapGraphQLOrderByDirectionToPrismaSortOrder';
 
 describe('GIVEN the mapGraphQLOrderByDirectionToPrismaSortOrder method', () => {
-  let graphQLOrderByDirection: OrderByDirection;
+  let orderByDirection: OrderByDirection | null | undefined;
+
+  describe('WHEN the order by direction is null', () => {
+    beforeEach(() => {
+      orderByDirection = null;
+    });
+
+    test('THEN it should return undefined', () => {
+      const response = mapGraphQLOrderByDirectionToPrismaSortOrder(orderByDirection);
+      expect(response).toBeUndefined();
+    });
+  });
+
+  describe('WHEN the order by direction is undefined', () => {
+    beforeEach(() => {
+      orderByDirection = undefined;
+    });
+
+    test('THEN it should return undefined', () => {
+      const response = mapGraphQLOrderByDirectionToPrismaSortOrder(orderByDirection);
+      expect(response).toBeUndefined();
+    });
+  });
 
   describe('WHEN the order by direction is ascending', () => {
     beforeEach(() => {
-      graphQLOrderByDirection = OrderByDirection.Ascending;
+      orderByDirection = OrderByDirection.Ascending;
     });
 
     test('THEN it should return the prisma sort order', () => {
-      const response = mapGraphQLOrderByDirectionToPrismaSortOrder(graphQLOrderByDirection);
+      const response = mapGraphQLOrderByDirectionToPrismaSortOrder(orderByDirection);
       expect(response).toEqual('asc');
     });
   });
 
   describe('WHEN the order by direction is descending', () => {
     beforeEach(() => {
-      graphQLOrderByDirection = OrderByDirection.Descending;
+      orderByDirection = OrderByDirection.Descending;
     });
 
     test('THEN it should return the prisma sort order', () => {
-      const response = mapGraphQLOrderByDirectionToPrismaSortOrder(graphQLOrderByDirection);
+      const response = mapGraphQLOrderByDirectionToPrismaSortOrder(orderByDirection);
       expect(response).toEqual('desc');
     });
   });
--- src/modules/order-by/helpers/mapGraphQLOrderByDirectionToPrismaSortOrder.ts
+++ src/modules/order-by/helpers/mapGraphQLOrderByDirectionToPrismaSortOrder.ts
@@ -6,6 +6,14 @@ import { SortOrder } from '@jest-games-organization/backend-package-prisma-types
  * @param o The GraphQL order by direction.
  * @returns The Prisma sort order.
  */
-export const mapGraphQLOrderByDirectionToPrismaSortOrder = (o: OrderByDirection): SortOrder => {
+export const mapGraphQLOrderByDirectionToPrismaSortOrder = (
+  o: OrderByDirection | null | undefined,
+): SortOrder | undefined => {
+  // If the order by direction is null or undefined, return undefined.
+  if (o === null || o === undefined) {
+    return undefined;
+  }
+
+  // Return the Prisma sort order.
   return o === OrderByDirection.Ascending ? 'asc' : 'desc';
 };
